8a8e08eaa0904d3f4e62f0830f9718a75a004365
Abdullah Zafar <zafar.abdullah10@gmail.com>
2023-04-10 13:35:10 -0400

    update vergence

--- splitTrials.m
+++ splitTrials.m
@@ -10,7 +10,7 @@ if idx_trial_AH(end, 2) > height(handles.tbl_AH)
     idx_trial_AH(end, 2) = height(handles.tbl_AH);
 end
 task_trials = struct;
-for i=1:size(idx_trial_EL,1)
+for i=1:min([size(idx_trial_EL,1), size(idx_trial_AH,1)])
     task_trials(i).EL = handles.tbl_EL(idx_trial_EL(i,1):idx_trial_EL(i,2), :);
     task_trials(i).AH = handles.tbl_AH(idx_trial_AH(i,1):idx_trial_AH(i,2), :);
     % DATA QUALITY
