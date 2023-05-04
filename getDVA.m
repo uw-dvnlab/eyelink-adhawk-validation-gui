fe4d68004054f1208537e835cd3f57a2f2ab047b
Abdullah Zafar <zafar.abdullah10@gmail.com>
2023-05-04 10:10:09 -0400

    add paper

--- getDVA.m
+++ getDVA.m
@@ -3,6 +3,7 @@ RES = [1024, 768]; % px
 DIM = [35.2, 26.4]; % cm
 DIST = 80; % cm
 POLARITY = 1;
+lbl_ah_z = ["Gaze_Z_Left", "Gaze_Z_Right"];
 
 if dim==1
     lbl_el = ["LX", "RX"];
@@ -24,7 +25,17 @@ EL_R = atan2d(d_dim(1) * ((raw_EL.(char(lbl_el(2))) / d_res(1)) - 0.5), DIST);
 % Adhawk
 AH_L = asind(raw_AH.(char(lbl_ah(1))));
 AH_R = asind(raw_AH.(char(lbl_ah(2))));
+% AH_Lt = atand(raw_AH.(char(lbl_ah(1))) ./ (-1*raw_AH.(char(lbl_ah_z(1)))));
+% AH_Rt = atand(raw_AH.(char(lbl_ah(2))) ./ (-1*raw_AH.(char(lbl_ah_z(2)))));
+AH_Lt = atan2d(raw_AH.(char(lbl_ah(1))), (-1*raw_AH.(char(lbl_ah_z(1)))));
+AH_Rt = atan2d(raw_AH.(char(lbl_ah(2))), (-1*raw_AH.(char(lbl_ah_z(2)))));
 
+if dim==1
+    assignin('base','AHL',[AH_L, AH_Lt]);
+    assignin('base','AHR',[AH_R, AH_Rt]);
+    
+    max([max(abs(diff([AH_R, AH_Rt], 1, 2))), max(abs(diff([AH_L, AH_Lt], 1, 2)))])
+end
 % Adjust
 if strcmp(task, 'HSP') || strcmp(task, 'VSP')
     mu_el_l = nanmean(EL_L(1:round(length(EL_L)/2)));
